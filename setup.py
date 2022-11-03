7b7bff5624548a0550c545ee31ed616e865dadac
master <maxvgv@gmail.com>
2022-11-03 18:36:19 +0100

    Original setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mx_fib_py",
+    version="0.0.1",
+    author="Maksym Vyhovskyi",
+    author_email="maxwell@gmail.com",
+    description="Calculates a Fibonacci number",
+    long_description="A basic library that calculates Fibonacci numbers",
+    long_description_content_type="text/markdown",
+    url="https://github.com/m-vyhovskyi/mx-fib-py.git",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    tests_require=['pytest'],
+)
