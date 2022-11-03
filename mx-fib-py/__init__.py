cde8beea53ebc3b7f31f5ccb8f81a8328b9f6b79
master <maxvgv@gmail.com>
2022-11-03 20:39:32 +0100

    Prepare for the first pip usage

---
+++ mx-fib-py/__init__.py
@@ -0,0 +1,2 @@
+def say_hello():
+    print("Mx Fibonacci module is saying hello")
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="mx_fib_py",
     version="0.0.1",
     author="Maksym Vyhovskyi",
     author_email="maxwell@gmail.com",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that calculates Fibonacci numbers",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/m-vyhovskyi/mx-fib-py.git",
     install_requires=[],
